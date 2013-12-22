235dac362b1c9f7c196d70841e3ba55e8cd848fb
Arne Franken <arne.franken@coremedia.com>
2013-12-22 22:58:01 +0100

    add HTMLPage test

---
+++ src/test/scala/basic/HtmlPageSimulation.scala
@@ -0,0 +1,60 @@
+package basic
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+import bootstrap._
+import assertions._
+
+/**
+ * Performance Test that requests URIs read from a sitemap
+ */
+class HtmlpageSimulation extends SimulationBase {
+
+  //read settings
+  val htmlpage: String = Option(System.getProperty("htmlpage")).getOrElse("schlagzeilen/")
+  val cssselector: String = Option(System.getProperty("cssselector")).getOrElse("div[class~=schlagzeilen-content] > a")
+
+  //create scenario, requests are executed for the configured duration for all URIs found in the Sitemap
+  val scn = scenario("HtmlPage")
+    //request sitemap, save all links found in URI_LIST
+    .exec(http(htmlpage)
+    .get(htmlpage)
+    .check(css(cssselector, "href").findAll.exists.saveAs("URI_LIST"))
+    )
+    .exec((s: Session) => {
+    logger.debug("URIs found: {}", s.attributes.get("URI_LIST"))
+    s
+  })
+    .doIfOrElse(session => session.contains("URI_LIST")) {
+    //crawl list for configured amount of time
+    during(testduration minutes) {
+      exec(
+        foreach("${URI_LIST}", "URI") {
+          exec(http("${URI}")
+            .get("${URI}")
+          )
+        }
+      )
+    }
+  } {
+    //no URIs found.
+    exec((s: Session) => {
+      logger.error("no URIs found.")
+      s
+    })
+  }
+
+  //actually start test
+  setUp(
+    scn.inject(
+      ramp(users users)
+        over (rampup seconds)
+    )
+  )
+    .protocols(httpConf)
+    .assertions(
+      global.successfulRequests.percent.is(successpercent),
+      details(baseUrl).responseTime.max.lessThan(maxresptime)
+    )
+}
